dbea28e0d47977c0bfff3ad3c6e9d1948872e106
louistru <louistru@hotmail.com>
2019-09-05 16:16:44 +0800

    up tools cp lmake install

--- tools/cp-lmake.js
+++ tools/cp-lmake.js
@@ -67,6 +67,7 @@ if (!fs.existsSync(target + '/bin/osx-jsa-shell')) {
 fs.chmodSync(target + '/gyp/gyp', 0755);
 
 copy_header(root + '/langou', `${include}/langou`);
+copy_header(root + '/lutils', `${include}/lutils`);
 copy_header(`${root}/depe/v8-link/include`, include);
 copy_header(`${root}/depe/node/deps/openssl/openssl/include/openssl`, `${include}/openssl`);
 copy_header(`${root}/depe/node/deps/openssl/config/archs`, `${include}/openssl/archs`);
