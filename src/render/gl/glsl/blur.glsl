66beec42db46b5594080936a8a990a0a20321fc6
louistru <louistru@hotmail.com>
2023-08-09 19:37:12 +0800

    update shader

--- src/render/gl/glsl/blur.glsl
+++ src/render/gl/glsl/blur.glsl
@@ -2,6 +2,7 @@
 // The solution is based on Wojciech Jarosz's Fast Image Convolution
 // https://blog.ivank.net/fastest-gaussian-blur.html
 // https://elynxsdk.free.fr/ext-docs/Blur/Fast_box_blur.pdf
+// https://www.peterkovesi.com/papers/FastGaussianSmoothing.pdf
 
 void main() {
 	gl_Position = matrix * vec4(vertex_in.xy, 0.0, 1.0);
