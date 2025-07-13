416e7f29a1fa0e3b40eef95eaeccc75a82af686b
louistru <louistru@hotmail.com>
2025-07-13 23:45:26 +0800

    up: doc tool

--- tools/gen_readme.js
+++ tools/gen_readme.js
@@ -401,7 +401,7 @@ function startExec(input,output) {
 		}
 
 		// Match to type: ([\w\[\]\<\>\|,\.\s]+)
-		let reg = /\W*(?:(export|static|protected|private|public)\s+)?(?:(async)\s+)?(?:(function|get|set)\s+)?(\w+)\s*(\<[^\>]+\>)?\s*\(([\w\[\]\<\>,\.\?:\|\s]*)\)\s*(?::\s*([\w\[\]\<\>\|,\.\s]+))?/my;
+		let reg = /\W*(?:(export|static|protected|private|public)\s+)?(?:(async|declare)\s+)?(?:(function|get|set)\s+)?(\w+)\s*(\<[^\>]+\>)?\s*\(([\w\[\]\<\>,\.\?:\|\s]*)\)\s*(?::\s*([\w\[\]\<\>\|,\.\s]+))?/my;
 		reg.lastIndex = lastIndex;
 		let mat = code.match(reg);
 		if (!mat)
