314f9cf2ad1caf15a864b935374412a9479270a9
louistru <louistru@hotmail.com>
2025-08-11 14:08:52 +0800

    fix: watching error

--- libs/quark/_watching.ts
+++ libs/quark/_watching.ts
@@ -39,7 +39,7 @@ let modules: Map<string, Module> = new Map();
 
 function markModuleObject(v: any, filename: string) {
 	if (v !== undefined) {
-		if (v && typeof v == 'object')
+		if (v/* && typeof v == 'object'*/)
 			v.__filename__ = filename;
 		return true;
 	}
